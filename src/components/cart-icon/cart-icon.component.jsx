5489c24f64efc8118a54d1a8218d5829cf199a41
Rick Middleton <ricksteruk@mac.com>
2020-02-14 10:27:56 +0000

    added checkout page, checout item and updated cart to use selectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
@@ -17,8 +18,8 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-const mapStatetoProps = state => ({
-    itemCount: selectCartItemsCount(state)
+const mapStatetoProps = createStructuredSelector({
+    itemCount: selectCartItemsCount
 });
 
 
