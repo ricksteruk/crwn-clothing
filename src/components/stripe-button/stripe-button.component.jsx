dde668a873cff05484ce15d05ed50010835139f7
Rick Middleton <ricksteruk@mac.com>
2020-02-15 12:31:39 +0000

    updating with stripe integration and our own favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import { connect } from 'react-redux';
+
+const StripeCheckoutButton = ({price}) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_3vs7O4v5084qF5pmN2FGUjuL00vhlsh4pN' 
+
+const onToken = token => {
+    console.log(token);
+    alert('Payment Successful');
+}
+
+    return (
+        <StripeCheckout 
+            label='Pay Now'
+            name='CRWN Clothing Ltd.'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is £${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    );
+
+};
+
+export default StripeCheckoutButton;
